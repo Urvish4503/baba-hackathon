d2a3cc829952fdc3f2c7e5b289c50f9043b34030
Harsh Butani <harshbutani2004@gmail.com>
2024-04-21 04:13:58 +0530

    ok

--- backend/src/routes/courses.ts
+++ backend/src/routes/courses.ts
@@ -4,18 +4,24 @@ import {
     courseDetail3,
     createCourse,
     createFaculty,
+    getSectionDetails,
+    storeSectionDetails,
 } from "../controllers/courses";
 
 const courseRoute = express.Router();
 
 // get course
 courseRoute.get("/summaryCourse", courseDetail1);
-courseRoute.get("/deatiledCourse", courseDetail3);
+courseRoute.get("/deatiledCourse/:courseId", courseDetail3);
 
 // add course
 courseRoute.post("/add", createCourse);
 
+// put quiz
+courseRoute.post("/sections/:sectionId/questions", storeSectionDetails);
+
 // get quiz
+courseRoute.get("/sections/:sectionId/questions", getSectionDetails);
 
 // tamparory
 courseRoute.post("/addFaculty", createFaculty);
