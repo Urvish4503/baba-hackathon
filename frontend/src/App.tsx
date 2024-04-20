8d289663311daf58b23c096f56c57ed59074fcef
Urvish <urvishp45003@gmail.com>
2024-04-20 15:04:15 +0530

    solved problem

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,33 @@ import NavBar from "./componants/NavBar";
 
 import Upload from "./componants/Upload";
 
+const Layout: FC = () => {
+    return (
+        <div className="app h-screen">
+            <NavBar />
+            <div className="flex flex-row h-3/4">
+                <Outlet />
+            </div>
+        </div>
+    );
+};
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <div>hi</div>,
+            },
+            // TODO: this shit
+            // {
+            //     path: "/contact",
+            //     element: <Contact />,
+            // },
+        ],
+    },
     {
         path: "/upload",
         element: <Upload />,
