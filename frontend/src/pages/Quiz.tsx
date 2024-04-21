6f1bb3259881903c898ab3ece5f0d48c7a343b29
Urvish <urvishp45003@gmail.com>
2024-04-21 13:36:10 +0530

    hdsoifasldkf

--- frontend/src/pages/Quiz.tsx
+++ frontend/src/pages/Quiz.tsx
@@ -2,26 +2,17 @@ import React, { useState, useEffect, FormEvent } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
-interface Question {
-    id: number;
-    question: string;
-    options: string[];
-    answer: string;
-}
-
 interface QuizParams extends Record<string, string | undefined> {
     courseId: string;
 }
 
 const QuizForm: React.FC = () => {
-    const [questions, setQuestions] = useState<Question[]>([]);
+    const [questions, setQuestions] = useState([]);
     const [responses, setResponses] = useState<{ [key: number]: string }>({});
 
     const { courseId } = useParams<QuizParams>();
 
     const courseIdNum = parseInt(courseId ?? "0", 10);
-
-    console.log("Course ID:", courseIdNum);
 
     useEffect(() => {
         // Fetch questions from the server when the component mounts
@@ -30,8 +21,8 @@ const QuizForm: React.FC = () => {
                 const response = await axios.get(
                     `http://localhost:8800/api/course/sections/1/questions`,
                 );
-                console.log(JSON.stringify(response.data))
-                setQuestions(response.data);
+                console.log(response.data.section.questions);
+                setQuestions(response.data.section.questions);
             } catch (error) {
                 console.error("Error fetching questions:", error);
             }
@@ -56,48 +47,42 @@ const QuizForm: React.FC = () => {
             <h2 className="text-3xl font-bold mt-4 mb-6">Quiz Form</h2>
             <form onSubmit={handleSubmit} className="w-full max-w-xl ">
                 <div className="bg-ctp-surface1 rounded-lg p-6">
-                    {questions.length > 0 ? (
-                        questions.map(question => (
-                            <div key={question.id} className="mb-8">
-                                <p className="text-lg font-semibold mb-2">
-                                    {question.id}. {question.question}
-                                </p>
-                                <ul className="pl-4">
-                                    {question.options.map((option, index) => (
-                                        <li key={index} className="mb-2">
-                                            <input
-                                                type="radio"
-                                                id={`option-${question.id}-${index}`}
-                                                name={`question-${question.id}`}
-                                                value={option}
-                                                onChange={e =>
-                                                    handleResponseChange(
-                                                        question.id,
-                                                        e.target.value,
-                                                    )
-                                                }
-                                                checked={
-                                                    responses[question.id] ===
-                                                    option
-                                                }
-                                                className="mr-2 focus:ring-ctp-blue"
-                                            />
-                                            <label
-                                                htmlFor={`option-${question.id}-${index}`}
-                                                className="text-ctp-subtext1"
-                                            >
-                                                {option}
-                                            </label>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        ))
-                    ) : (
-                        <p className="text-ctp-subtext0">
-                            Loading questions...
-                        </p>
-                    )}
+                    {questions.map(question => (
+                        <div key={question.id} className="mb-8">
+                            <p className="text-lg font-semibold mb-2">
+                                {question.id}. {question.question}
+                            </p>
+                            <ul className="pl-4">
+                                {question.options.map((option, index) => (
+                                    <li key={index} className="mb-2">
+                                        <input
+                                            type="radio"
+                                            id={`option-${question.id}-${index}`}
+                                            name={`question-${question.id}`}
+                                            value={option}
+                                            onChange={e =>
+                                                handleResponseChange(
+                                                    question.id,
+                                                    e.target.value,
+                                                )
+                                            }
+                                            checked={
+                                                responses[question.id] ===
+                                                option
+                                            }
+                                            className="mr-2 focus:ring-ctp-blue"
+                                        />
+                                        <label
+                                            htmlFor={`option-${question.id}-${index}`}
+                                            className="text-ctp-subtext1"
+                                        >
+                                            {option}
+                                        </label>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
                 </div>
                 <div className="mt-6 flex justify-center"></div>
             </form>
